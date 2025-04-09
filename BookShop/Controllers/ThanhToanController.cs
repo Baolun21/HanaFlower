fb6370907ee887a87e1784999b893e6db7a2c885
tican10302 <tricuong10302@gmail.com>
2025-04-09 21:41:41 +0700

    Fix: bill empty

--- BookShop/Controllers/ThanhToanController.cs
+++ BookShop/Controllers/ThanhToanController.cs
@@ -1,11 +1,25 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using System.Globalization;
+using Microsoft.AspNetCore.Mvc;
 
 namespace BookShop.Controllers
 {
     public class ThanhToanController : Controller
     {
-        public IActionResult Index()
+        [HttpPost]
+        public IActionResult Index(string TenKH, string SDT, string Email, string DiaChiGiao, string note, decimal TongTien)
         {
+            // Truyền dữ liệu đến view
+            ViewBag.TenKH = TenKH ?? "";
+            ViewBag.SDT = SDT ?? "";
+            ViewBag.Email = Email ?? "";
+            ViewBag.DiaChiGiao = DiaChiGiao ?? "";
+            ViewBag.note = note ?? "";
+            ViewBag.TongTien = TongTien;
+            
+        
+            // Lưu tổng tiền dưới dạng string do Session chỉ lưu được string
+            HttpContext.Session.SetString("TongTien", TongTien.ToString(CultureInfo.InvariantCulture));
+
             return View();
         }
     }
